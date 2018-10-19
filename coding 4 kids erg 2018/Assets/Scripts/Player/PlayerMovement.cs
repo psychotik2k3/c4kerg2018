15c1114dffa861ba202832a79a0e95adeef9791d
bornus <Sebastien.Bornu@moodys.com>
2018-10-19 17:07:06 +0200

    added player transitions and movement code

--- coding 4 kids erg 2018/Assets/Scripts/Player/PlayerMovement.cs
+++ coding 4 kids erg 2018/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,36 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float speed = 6f;
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+    }
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+
+    }
 }
